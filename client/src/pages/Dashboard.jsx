848087fffa5c18df5b15dc162f4480352c54408f
hiraksaharia <hiraksaharia4@gmail.com>
2024-03-05 11:13:27 +0530

    added posts section to the Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
+import DashPost from "../components/DashPosts";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -23,6 +24,9 @@ export default function Dashboard() {
       </div>
       {/* Profile... */}
       {tab === "profile" && <DashProfile />}
+
+      {/* posts... */}
+      {tab === "posts" && <DashPost />}
     </div>
   );
 }
