990a1bd9431e3557d4c5c37e10de10f364e544d5
hiraksaharia <hiraksaharia4@gmail.com>
2024-03-10 11:57:51 +0530

    Created get posts api route

--- API/controllers/post.controller.js
+++ API/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import { now } from "mongoose";
 import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -29,5 +30,51 @@ export const create = async (req, res, next) => {
     res.status(201).json(savedPost);
   } catch (error) {
     next(error);
+  }
+};
+
+export const getposts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { userId: req.query.category }),
+      ...(req.query.slug && { userId: req.query.slug }),
+      ...(req.query.postId && { _Id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    // get total number of posts
+    const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    //create the response and send back all these information
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+  } catch (error) {
+    next(errorHandler);
   }
 };
